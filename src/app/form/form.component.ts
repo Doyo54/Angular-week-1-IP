dc58233c81ba1eef532853b80b94e386343393f0
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-04-08 20:45:26 +0300

    ft: Post Quote button added and functional

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+  onPost(name: HTMLInputElement, author: HTMLInputElement, quote: HTMLInputElement, date: HTMLInputElement): boolean {
+     console.log(`Adding article title: ${name.value} and link: ${author.value}`);
+     return false;
+  }
   constructor() { }
 
   ngOnInit(): void {
