a04dc3185943f35cca671aa6f8d9f0d31e0057ab
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-04-11 13:14:35 +0300

    ft: I have added styling to my buttons

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -13,7 +13,7 @@ transform(value: any): number {
     var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
     var dateCounter = dateDifferenceSeconds/secondsInDay;
 
-    if (dateCounter >= 1 && value < todayWithNoTime){
+    if (dateCounter >= 1 &&  todayWithNoTime > value){
       return dateCounter;
     }else{
       return 0;
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,7 @@
 export class Quote {
     public votesUp:number
     public votesDown:number
+    public length:any
     constructor(public name:string, public Author:string,public quote:string, public date:Date){
         this.votesUp =0;
         this.votesDown =0;
